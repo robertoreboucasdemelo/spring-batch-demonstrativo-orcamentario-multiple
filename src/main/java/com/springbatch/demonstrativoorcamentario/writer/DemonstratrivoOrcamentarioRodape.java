31c94ae3d9f71c571cdcfaeaf821bb501c5b4408
Roberto Reboucas de Melo <roberto.reboucas@ibm.com>
2022-12-16 16:46:08 -0300

    Add Project

---
+++ src/main/java/com/springbatch/demonstrativoorcamentario/writer/DemonstratrivoOrcamentarioRodape.java
@@ -0,0 +1,33 @@
+package com.springbatch.demonstrativoorcamentario.writer;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.text.NumberFormat;
+import java.util.List;
+
+import com.springbatch.demonstrativoorcamentario.dominio.GrupoLancamento;
+
+import org.springframework.batch.core.annotation.AfterChunk;
+import org.springframework.batch.core.annotation.BeforeWrite;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.item.file.FlatFileFooterCallback;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DemonstratrivoOrcamentarioRodape implements FlatFileFooterCallback {
+  private Double totalGeral = 0.0;
+
+  @Override
+  public void writeFooter(Writer writer) throws IOException {
+    writer.append("\n");
+    writer.append(String.format("\t\t\t\t\t\t\t  Total: %s\n", NumberFormat.getCurrencyInstance().format(totalGeral)));
+    writer.append(String.format("\t\t\t\t\t\t\t  Código de Autenticação: %s\n", "fkyew6868fewjfhjjewf"));
+  }
+  
+  @BeforeWrite
+  public void beforeWrite(List<GrupoLancamento> grupos) {
+    for (GrupoLancamento grupoLancamento : grupos) {
+      totalGeral += grupoLancamento.getTotal();
+    }
+  }
+}
